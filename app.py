2af4253860dad74f0994f5eb8d6864aa174b5685
laiz-in <laizin2107@gmail.com>
2023-07-20 12:32:58 +0400

    all redirections in website are added

--- app.py
+++ app.py
@@ -5,33 +5,47 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    #home page or just landing page
+    #home page
     return render_template('home.html')
 
 
-@app.route('/about')
-def about():
-    home_url = url_for('home')
 
-    #contents that contains in about page
-    return render_template('about.html',home_url=home_url)
-
+@app.route('/ReadMoreAboutAI')
+def ReadMoreAboutAI():
+    #page that deals with AI in healthcares section
+    return render_template('ReadMoreAboutAI.html')
 
 @app.route('/bmi')
 def bmi():
     #page that expects height and weight and returns the BMI
     return render_template('bmi.html')
 
-@app.route('/heartrate')
-def heartrate():
+@app.route('/heartdisease')
+def heartdisease():
     #page that deals with heartrates
-    return render_template('heartrate.html')
+    return render_template('heartdisease.html')
 
 
-@app.route('/xray')
-def xray():
+@app.route('/pneumonia')
+def pneumonia():
     #page that deals with heartrates
-    return render_template('xray.html')
+    return render_template('pneumonia.html')
+
+@app.route('/skindisease')
+def skindisease():
+    #page that deals with heartrates
+    return render_template('skindisease.html')
+
+@app.route('/diabeticdisease')
+def diabeticdisease():
+    #page that deals with heartrates
+    return render_template('diabeticdisease.html')
+
+@app.route('/diabeticretinopathy')
+def diabeticretinopathy():
+    #page that deals with heartrates
+    return render_template('diabeticretinopathy.html')
+
 
 
 
